7479e0416b92da18e82a16221737984d8e4409aa
Maël Penicaud <91068627+MarlBnc@users.noreply.github.com>
2023-07-19 12:09:04 +0200

    Update random.py

--- random.py
+++ random.py
@@ -2,12 +2,15 @@ import numpy as np
 
 numbers = np.arange(151)
 proba = {
-    'common' : (0.748, 90),
+    'common' : (0.749, 90),
     'rare' : (0.15, 35),
     'epique' : (0.075, 15),
     'legendaire' : (0.025, 10),
-    'ultime' : (0.002, 1)
+    'ultime' : (0.001, 1)
 }
+PRICE = 450
+N_CARD_PER_PACK = 4
+N_SIMU = 2000
 
 card_vect = [[name]*n[1] for name,n in proba.items()]
 card_vect = np.hstack(card_vect)
@@ -19,7 +22,7 @@ for l in proba.keys():
     results[l] = []
 results['cartes'] = []
 
-N_SIMU = 500
+
 
 for simu in range(N_SIMU):
     unique_pick = []
@@ -28,7 +31,7 @@ for simu in range(N_SIMU):
     i = 0
     while len(unique_pick) != len(numbers):
         i += 1
-        pack = np.random.choice(numbers,4, replace=False, p=proba_vect)
+        pack = np.random.choice(numbers, N_CARD_PER_PACK, replace=False, p=proba_vect)
         for n in pack:
             if n not in unique_pick:
                 unique_pick.append(n)
@@ -49,7 +52,7 @@ for simu in range(N_SIMU):
         results[l].append(n)
 
 for l, v in results.items():
-    print(f'Toutes les {l} ont été obtenus en {np.mean(v)} packs en moyennne')
+    print(f'Toutes les {l} ont été obtenus en {np.mean(v):.3g} packs en moyennne : {round(np.mean(v))*PRICE}$')
 
 
 
