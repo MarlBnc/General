8a1817dd972a36b922d558a622cedf92054a7e77
Maël Penicaud <91068627+MarlBnc@users.noreply.github.com>
2023-07-19 11:06:55 +0200

    Add files via upload

---
+++ random.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+numbers = np.arange(151)
+proba = {
+    'common' : (0.748, 90),
+    'rare' : (0.15, 35),
+    'epique' : (0.075, 15),
+    'legendaire' : (0.025, 10),
+    'ultime' : (0.002, 1)
+}
+
+card_vect = [[name]*n[1] for name,n in proba.items()]
+card_vect = np.hstack(card_vect)
+proba_vect =  [[p/n]*n for p,n in proba.values()]
+proba_vect = np.hstack(proba_vect)
+
+results = {}
+for l in proba.keys():
+    results[l] = []
+results['cartes'] = []
+
+N_SIMU = 500
+
+for simu in range(N_SIMU):
+    unique_pick = []
+    found = []
+    n_packs = []
+    i = 0
+    while len(unique_pick) != len(numbers):
+        i += 1
+        pack = np.random.choice(numbers,4, replace=False, p=proba_vect)
+        for n in pack:
+            if n not in unique_pick:
+                unique_pick.append(n)
+        unique_values, counts = np.unique(card_vect[unique_pick], return_counts=True)
+        tirage = {}
+        for l, c in zip(unique_values, counts):
+            tirage[l] = c
+        for level in unique_values:
+            if proba[level][1] == tirage[level] and level not in found:
+                found.append(level)
+                n_packs.append(i)
+                # print(f'Toutes les {level} ont été obtenus en {i} pack')
+    found.append('cartes')
+    n_packs.append(i)
+    # print(f'toutes les cartes ont été trouvées en {i} paquet\n')
+
+    for l, n in zip(found,n_packs):
+        results[l].append(n)
+
+for l, v in results.items():
+    print(f'Toutes les {l} ont été obtenus en {np.mean(v)} packs en moyennne')
+
+
+
+
